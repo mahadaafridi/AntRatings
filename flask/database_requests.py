17b1332a8fc12875bcbaeff5e71ea5e7e05d93c2
Mahad Afridi <mahadakbarafridi@gmail.com>
2024-03-25 14:21:19 -0700

    Return reviews

--- flask/database_requests.py
+++ flask/database_requests.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS
 
+import json
 #import statements for mongo:
 from flask import Flask
 from pymongo import MongoClient
+from bson.json_util import dumps
+
 
 # client = MongoClient("mongodb+srv://mukundprnair:YiYWnyTFP01RLbWc@cluster0.habvoku.mongodb.net/")
 client = MongoClient("mongodb+srv://mahadakbarafridi:awvnF4jCC9NTztHV@cluster0.gppjo6c.mongodb.net/")
@@ -14,13 +17,13 @@ class_data = {
     'department' : "CS",
     'course_num': "171",
     'prof_name': 'Kask',
-    'difficulty': 2,
-    'hrs_per_week': 10,
-    'text': "Boring class"
+    'difficulty': 9,
+    'hrs_per_week': 24,
+    'text': "Enjoyed the class. Bad professor"
 }
 
 
-result = db.classes.insert_one(class_data)
+#result = db.classes.insert_one(class_data)
 print("Done")
 
 
@@ -31,7 +34,7 @@ def getReviews(department: str, course_number: str) -> jsonify:
     sample review entry in Mongo:
     {
         'department':
-        'course_number':
+        'course_num':
         'difficulty':
         'hrs_per_week':
         'text':
@@ -42,13 +45,29 @@ def getReviews(department: str, course_number: str) -> jsonify:
     db = client['classes_data']
 
     
-    query = {'department': department, 'course_number' : course_number}
+    query = {'department': department, 'course_num' : course_number}
     matching_reviews = db.classes.find(query)
+    #print(type(matching_reviews))
+    #print(matching_reviews)
+    # reviews = list()
+    # for review in matching_reviews:
+    #     print("reach")
+    #     print(review)
+    #     reviews.append(review)
+        
 
-    print(matching_reviews.json())
+    # print(json.dumps(reviews))
+    reviews = list(matching_reviews)
+    print(type(dumps(reviews)))
+    
+    #json_data = json.dumps(matching_reviews.next())
+    #print(json_data)
 
-# if __name__ == '__main__':
-    #getReviews('I&C SCI', '6B')
+
+    
+
+if __name__ == '__main__':
+    getReviews('CS', '171')
 
 
     
