59c52a1de99e69ffd7fd8cc2a7a7063e0ba13c76
puruvian <hellojunior2021@gmail.com>
2024-03-25 12:29:43 -0700

    started adding department options

--- ant-ratings/src/user_input.js
+++ ant-ratings/src/user_input.js
@@ -41,12 +41,79 @@ const UserInputComponent = () => {
         <Form.Group className="col-auto">
           <Dropdown onSelect={handleSelect}>
             <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-              Select an Item
+              {selectedItem}
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item eventKey="Item 1">Item 1</Dropdown.Item>
-              <Dropdown.Item eventKey="Item 2">Item 2</Dropdown.Item>
-              <Dropdown.Item eventKey="Item 3">Item 3</Dropdown.Item>
+              <Dropdown.Item eventKey="AC ENG">AC ENG</Dropdown.Item>
+              <Dropdown.Item eventKey="AFAM">AFAM</Dropdown.Item>
+              <Dropdown.Item eventKey="ANATOMY">ANATOMY</Dropdown.Item>
+              <Dropdown.Item eventKey="ANESTH">ANESTH</Dropdown.Item>
+              <Dropdown.Item eventKey="ANTHRO">ANTHRO</Dropdown.Item>
+              <Dropdown.Item eventKey="ARABIC">ARABIC</Dropdown.Item>
+              <Dropdown.Item eventKey="ARMN">ARMN</Dropdown.Item>
+              <Dropdown.Item eventKey="ART">ART</Dropdown.Item>
+              <Dropdown.Item eventKey="ART HIS">ART HIS</Dropdown.Item>
+              <Dropdown.Item eventKey="ARTS">ARTS</Dropdown.Item>
+              <Dropdown.Item eventKey="ARTSHUM">ARTSHUM</Dropdown.Item>
+              <Dropdown.Item eventKey="ASIANAM">ASIANAM</Dropdown.Item>
+              <Dropdown.Item eventKey="BANA">BANA</Dropdown.Item>
+              <Dropdown.Item eventKey="BATS">BATS</Dropdown.Item>
+              <Dropdown.Item eventKey="BIO SCI">BIO SCI</Dropdown.Item>
+              <Dropdown.Item eventKey="BIOCHEM">BIOCHEM</Dropdown.Item>
+              <Dropdown.Item eventKey="BME">BME</Dropdown.Item>
+              <Dropdown.Item eventKey="CAMPREC">CAMPREC</Dropdown.Item>
+              <Dropdown.Item eventKey="CEM">CEM</Dropdown.Item>
+              <Dropdown.Item eventKey="CHC/LAT">CHC/LAT</Dropdown.Item>
+              <Dropdown.Item eventKey="CHEM">CHEM</Dropdown.Item>
+              <Dropdown.Item eventKey="CHINESE">CHINESE</Dropdown.Item>
+              <Dropdown.Item eventKey="CLASSIC">CLASSIC</Dropdown.Item>
+              <Dropdown.Item eventKey="CLT&THY">CLT&THY</Dropdown.Item>
+              <Dropdown.Item eventKey="COGS">COGS</Dropdown.Item>
+              <Dropdown.Item eventKey="COM LIT">COM LIT</Dropdown.Item>
+              <Dropdown.Item eventKey="COMPSCI">COMPSCI</Dropdown.Item>
+              <Dropdown.Item eventKey="CRITISM">CRITISM</Dropdown.Item>
+              <Dropdown.Item eventKey="CRM/LAW">CRM/LAW</Dropdown.Item>
+              <Dropdown.Item eventKey="CSE">CSE</Dropdown.Item>
+              <Dropdown.Item eventKey="DANCE">DANCE</Dropdown.Item>
+              <Dropdown.Item eventKey="DATA">DATA</Dropdown.Item>
+              <Dropdown.Item eventKey="DERM">DERM</Dropdown.Item>
+              <Dropdown.Item eventKey="DEV BIO">DEV BIO</Dropdown.Item>
+              <Dropdown.Item eventKey="DRAMA">DRAMA</Dropdown.Item>
+              <Dropdown.Item eventKey="EARTHSS">EARTHSS</Dropdown.Item>
+              <Dropdown.Item eventKey="ECO EVO">ECO EVO</Dropdown.Item>
+              <Dropdown.Item eventKey="ECON">ECON</Dropdown.Item>
+              <Dropdown.Item eventKey="ECPS">ECPS</Dropdown.Item>
+              <Dropdown.Item eventKey="ED AFF">ED AFF</Dropdown.Item>
+              <Dropdown.Item eventKey="EDUC">EDUC</Dropdown.Item>
+              <Dropdown.Item eventKey="EECS">EECS</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+              <Dropdown.Item eventKey="Item">Select</Dropdown.Item>
+
             </Dropdown.Menu>
           </Dropdown>
           {selectedItem && <p>You selected: {selectedItem}</p>}
